ea62f003d8ce86801e173d22bfd84e95b61ef5cd
bc-uh <chern@hartford.edu>
2023-10-11 08:44:55 -0400

    Set up scanner

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int diamondSize = scanner.nextInt();
   }
 }
