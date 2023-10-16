c473903c6e63a55b6be735042b8e01dc770dbe88
bc-uh <chern@hartford.edu>
2023-10-16 00:52:53 -0400

    Print first row of even diamond

--- Diamond.java
+++ Diamond.java
@@ -4,5 +4,11 @@ public class Diamond {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int diamondSize = scanner.nextInt();
+    if (diamondSize % 2 == 0) {
+      for (int i = 0; i < diamondSize; i++) {
+        System.out.print(" ");
+      }
+      System.out.print("*");
+    }
   }
 }
