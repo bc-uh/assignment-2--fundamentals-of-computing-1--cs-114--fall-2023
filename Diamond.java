26022fa86e05d97c0d72b5ed0607503846cf5b99
bc-uh <chern@hartford.edu>
2023-10-16 02:13:44 -0400

    Add comments

--- Diamond.java
+++ Diamond.java
@@ -4,13 +4,18 @@ public class Diamond {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Enter a number");
-    int diamondSize = scanner.nextInt();
-    scanner.close();
+    int diamondSize = scanner.nextInt(); // User enters diamond size
+    scanner.close(); // Close scanner to save resources
     if (diamondSize % 2 == 0) {
+      // Even diamond
+
+      // Print top *
       for (int i = 0; i < diamondSize; i++) {
         System.out.print(" ");
       }
       System.out.print("*\n");
+
+      // Print top half ("j" loops print spaces and asterisks, i loop is the row)
       for (int i = 1; i < diamondSize / 2; i++) {
         for (int j = diamondSize - (i * 2); j >= 0; j--) {
           System.out.print(" ");
@@ -20,6 +25,8 @@ public class Diamond {
         }
         System.out.println();
       }
+
+      // Print bottom half
       for (int i = diamondSize / 2; i > 0; i--) {
         for (int j = diamondSize - (i * 2); j >= 0; j--) {
           System.out.print(" ");
@@ -29,11 +36,16 @@ public class Diamond {
         }
         System.out.println();
       }
+
+      // Print bottom *
       for (int i = 0; i < diamondSize; i++) {
         System.out.print(" ");
       }
       System.out.print("*");
     } else {
+      // Odd diamond
+
+      // Print top half
       for (int i = 0; i < diamondSize / 2; i++) {
         for (int j = diamondSize / 2 - i; j > 0; j--) {
           System.out.print(" ");
@@ -43,6 +55,8 @@ public class Diamond {
         }
         System.out.println();
       }
+
+      // Print bottom half
       for (int i = diamondSize / 2; i >= 0; i--) {
         for (int j = diamondSize / 2 - i; j > 0; j--) {
           System.out.print(" ");
